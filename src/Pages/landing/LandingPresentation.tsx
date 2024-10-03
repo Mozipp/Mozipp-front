caa063da454a48e98da96112ba62588bbedd32df
seungwon <dasusam84@gmail.com>
2024-10-03 18:36:13 +0900

    test

--- src/Pages/landing/LandingPresentation.tsx
+++ src/Pages/landing/LandingPresentation.tsx
@@ -36,7 +36,7 @@ const LandingPresentation=(props:any)=>{
     return (
 
         <div>
-        <Button>로그인</Button>
+        <Button>회원가입</Button>
         <Editable
         textAlign='center'
         defaultValue='Rasengan ⚡️'
