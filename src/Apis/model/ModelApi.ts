49bc181e71c9f50e78d16bedd2c6abc2a06bc0f7
Keonyoung <joseph12752@gmail.com>
2024-11-25 01:13:40 +0900

    API divided

---
+++ src/Apis/model/ModelApi.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+
+// Axios 인스턴스 생성
+const api: AxiosInstance = axios.create({
+  baseURL: "https://api.multi-learn.com/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 쿠키에서 accessToken 추출 함수
+const getAccessTokenFromCookies = (): string | null => {
+  const cookies = document.cookie.split("; ");
+  const accessTokenCookie = cookies.find((cookie) => cookie.startsWith("access_token="));
+  return accessTokenCookie ? accessTokenCookie.split("=")[1] : null;
+};
+
+// Axios 요청 인터셉터 설정
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = getAccessTokenFromCookies();
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
